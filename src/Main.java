ce6d54ed8613fad1317e9787f0cda5b09f463440
PedroHMantis <gtx1050pd@gmail.com>
2025-08-24 13:27:47 -0300

    Streams

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import entities.Employee;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter full file path: ");
+        String path = sc.nextLine();
+
+        System.out.print("Enter salary: ");
+        Double auxsalary = sc.nextDouble();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+            String line = br.readLine();
+
+            List<Employee> list = new ArrayList<>();
+
+            while(line != null){
+                String [] fields = line.split(",");
+                String name = fields[0];
+                String email = fields [1];
+                Double salary = Double.parseDouble(fields[2]);
+
+                list.add(new Employee(name, email, salary));
+
+                line = br.readLine();
+
+            }
+
+            System.out.printf("Email of people whose salary is more than %.2f%n", auxsalary);
+
+            List<String> names = list.stream()
+                    .filter(e -> e.getSalary() >= auxsalary)
+                    .map(e -> e.getEmail())
+                    .collect(Collectors.toList());
+
+            names.forEach(System.out::println);
+
+            Double msalary = list.stream()
+                    .filter(e-> e.getName().charAt(0) == 'M')
+                    .map(e -> e.getSalary())
+                    .reduce(0.0,(x,y) -> x + y);
+
+            System.out.println("Sum of salary of people whose name starts with 'M': " + String.format("%.2f", msalary));
+        }
+        catch (IOException e ){
+            System.out.println("ERROR: " + e.getMessage());
+        }
+
+
+        sc.close();
+    }
+}
